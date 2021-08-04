d93a9d1d09f5e6c2eff065c30bbe3111a0ad2e47
Brian Parks <bparks@brianparks.me>
2021-08-03 21:21:42 -0600

    Test between and null

--- test/PredicateTest.php
+++ test/PredicateTest.php
@@ -10,4 +10,16 @@ class PredicateTest extends PHPUnit\Framework\TestCase
         $params = [];
         $this->assertEquals(Q::equals(1, 1)->toAnsiSql($params), "1 = 1");
     }
+
+    public function test_between()
+    {
+        $params = [];
+        $this->assertEquals(Q::between(2, 1, 3)->toAnsiSql($params), "2 between 1 and 3");
+    }
+
+    public function test_null()
+    {
+        $params = [];
+        $this->assertEquals(Q::isNull('col_name')->toAnsiSql($params), "col_name is null");
+    }
 }
